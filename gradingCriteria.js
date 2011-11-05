8eccc1093c05299c93a65051efc149d8ff9e4161
donabrams <donabrams@gmail.com>
2011-11-05 14:11:04 -0400

    Grading Criteria complete. Switched to a circumference proportional method of calculating the pie sections and it worked like a charm! Looks beautiful.

--- gradingCriteria.js
+++ gradingCriteria.js
@@ -1,10 +1,10 @@
 $(function(){
 	var criteria = $("#grading_criteria li");
 	if (criteria.length) {
-		var graphRadius = 100.0;
-		var innerRadius = 45.0;
-		var margin = 40.0;
-		var offset = 8.0;
+		var graphRadius = 105.0;
+		var innerRadius = 50.0;
+		var margin = 30.0;
+		var offset = 2.0;
 		var theta0 = Math.PI/5.0;
 		
 		var $legend = $("#grading_criteria .legend");
@@ -24,10 +24,6 @@ $(function(){
 				.svg({settings: {'class':'pie','width':graphDiameter, 'height':graphDiameter }, 
 						onLoad: function(svg) {
 							var g = svg.group({'stroke-width': 0, id:"grading_criteria_pie"});
-							var origin = translateToOrigin({x:0,y:0});
-							svg.ellipse(g, origin.x, origin.y, innerRadius+offset, innerRadius+offset, {'fill':'red'});
-							svg.ellipse(g, origin.x, origin.y, innerRadius, innerRadius, {'fill':'green'});
-							svg.ellipse(g, origin.x, origin.y, offset, offset, {'fill':'blue'});
 							// little circles around graph
 							var br = graphRadius + offset + 2.0;
 							var bc = 2.0* Math.PI * br;
@@ -40,32 +36,35 @@ $(function(){
 							}
 							// pie chart slices
 							var percentTotal = 0.0;
+							var innerCircumference = 2.0 * Math.PI * innerRadius;
+							var graphCircumference = 2.0 * Math.PI * graphRadius;
+							// offset is not quite spacing-- that would require solving for the arclength that
+							// would create the given spacing and using that here instead of offset
+							var lastTheta = theta0;
+							var outerOffsetAngle = offset / graphCircumference * 2.0 * Math.PI;
+							var innerOffsetAngle = offset / innerCircumference * 2.0 * Math.PI;
 							criteria.each(function(i, d) {
 								var percent = $("meter", d).attr("value");
-								var theta1 = (pToR(percentTotal) + theta0);
-								var theta2 = (pToR(percentTotal) + pToR(percent) + theta0);
-								var theta3 = (pToR(percentTotal) + pToR(percent)/2 + theta0);
+								var theta = pToR(percent);
+								var theta1i = lastTheta + innerOffsetAngle;
+								var theta1o = lastTheta + outerOffsetAngle;
+								var theta2i = theta1i + theta - 2 * innerOffsetAngle;
+								var theta2o = theta1o + theta - 2 * outerOffsetAngle;
+								lastTheta = lastTheta + theta;
+								
 								var r1 = innerRadius;
 								var r2 = graphRadius;
-								var r3 = offset;
-								var po = {'x': r3 * Math.cos(theta3), 'y': r3 * Math.sin(theta3)};
-								var p1 = translateToOrigin({'x': r1 * Math.cos(theta1) + po.x, 'y': r1 * Math.sin(theta1) + po.y});
-								var p2 = translateToOrigin({'x': r1 * Math.cos(theta2) + po.x, 'y': r1 * Math.sin(theta2) + po.y});
-								var p3 = translateToOrigin({'x': r2 * Math.cos(theta2) + po.x, 'y': r2 * Math.sin(theta2) + po.y});
-								var p4 = translateToOrigin({'x': r2 * Math.cos(theta1) + po.x, 'y': r2 * Math.sin(theta1) + po.y});
-								
-								svg.ellipse(g, translateToOrigin(po).x, translateToOrigin(po).y, 1, 1);
-								svg.ellipse(g, p1.x, p1.y, 1, 1);
-								svg.ellipse(g, p2.x, p2.y, 1, 1);
-								svg.ellipse(g, p3.x, p3.y, 1, 1);
-								svg.ellipse(g, p4.x, p4.y, 1, 1);
+								var p1 = translateToOrigin({'x': r1 * Math.cos(theta1i), 'y': -(r1 * Math.sin(theta1i))});
+								var p2 = translateToOrigin({'x': r1 * Math.cos(theta2i), 'y': -(r1 * Math.sin(theta2i))});
+								var p3 = translateToOrigin({'x': r2 * Math.cos(theta2o), 'y': -(r2 * Math.sin(theta2o))});
+								var p4 = translateToOrigin({'x': r2 * Math.cos(theta1o), 'y': -(r2 * Math.sin(theta1o))});
 								
 								var cssClass = $(d).attr("class");
 								var slice = svg.createPath();
 								slice.move(p1.x, p1.y);
-								slice.arc(innerRadius+r3, innerRadius+r3, 0, percent > 50.0 ? 1 : 0, 1, p2.x, p2.y);
+								slice.arc(innerRadius, innerRadius, 0, percent > 50.0 ? 1 : 0, 0, p2.x, p2.y);
 								slice.line(p3.x, p3.y);
-								slice.arc(graphRadius+r3, graphRadius+r3, 0, percent > 50.0 ? 1 : 0, 0, p4.x, p4.y);
+								slice.arc(graphRadius, graphRadius, 0, percent > 50.0 ? 1 : 0, 1, p4.x, p4.y);
 								slice.close();
 								svg.path(g, slice, {'class':cssClass});
 								percentTotal += percent;
@@ -73,9 +72,10 @@ $(function(){
 		}});
 		var aside = $("#grading_criteria aside");
 		aside.css({'position': 'absolute', 
-				'right': (margin+graphRadius-innerRadius/2) + "px", 
-				'bottom': (margin+graphRadius-innerRadius/2) + "px", 
+				'right': (graphRadius+margin-innerRadius/Math.sqrt(2)) + "px", 
+				'bottom': (graphRadius+margin-innerRadius/Math.sqrt(2)) + "px", 
 				'display': 'block',
-				'width' : innerRadius*2});
+				'width' : 2*innerRadius/Math.sqrt(2),
+				'height' : 2*innerRadius/Math.sqrt(2)});
 	}
 });
